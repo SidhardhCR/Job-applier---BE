b0322fae6ce0a16e22aa8677d7710a7319360c13
unknown <naseefp.hin@gmail.com>
2025-01-28 13:20:35 +0530

    initial commit

---
+++ db.py
@@ -0,0 +1,13 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+class Database:
+    def __init__(self, uri: str):
+        self.client = AsyncIOMotorClient(uri)
+        self.db = self.client["JobFinder"]
+
+    def get_collection(self, collection_name: str):
+        return self.db[collection_name]
+
+# Initialize DB connection
+db = Database(os.getenv("MONGO_DETAILS"))
